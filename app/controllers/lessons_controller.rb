a44dfc2e71f157fc9221e84dcc84fb2662b78499
codingwollf <anu.garcha12@gmail.com>
2019-06-24 01:37:45 +0000

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,22 @@
 class LessonsController < ApplicationController
-	
-  def show
-  end
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_lesson
 
-  private
+def show
+end
 
-  helper_method :current_lesson
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
+private
+
+def require_authorized_for_current_lesson
+	if current_user.enrolled_in?(current_lesson.section.course) == false && current_lesson.section.course.user != current_user
+		flash[:alert] = "Wait up! You are not enrolled in this course. You need to enroll first to view any more lessons. Thank you!"
+		redirect_to course_path(current_lesson.section.course)
+	end
+end
+
+helper_method :current_lesson
+def current_lesson
+	@current_lesson ||= Lesson.find(params[:id])
+end
+
 end
