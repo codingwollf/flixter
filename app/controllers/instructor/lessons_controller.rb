7fefeb22f6292700876f0bd80763be9582a5fe8c
codingwollf <anu.garcha12@gmail.com>
2019-07-02 22:50:13 +0000

    lesson 31

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,9 +1,8 @@
 class Instructor::LessonsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:create]
+  before_action :require_authorized_for_current_section, only: [:new, :create]
   before_action :require_authorized_for_current_lesson, only: [:update]
-
   def new
     @lesson = Lesson.new
   end
@@ -13,7 +12,22 @@ class Instructor::LessonsController < ApplicationController
     redirect_to instructor_course_path(current_section.course)
   end
 
+  def update
+    current_lesson.update_attributes(lesson_params)
+    render plain: 'updated!'
+  end
+
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 
   def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
@@ -37,6 +51,6 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video)
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
