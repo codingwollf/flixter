4586b6ba2e2e70909b96633fffe527f16521748e
codingwollf <anu.garcha12@gmail.com>
2019-07-03 00:58:01 +0000

    New section form modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
-  get 'privacy', to: 'static_pages#privacy'
-  get 'team', to: 'static_pages#team'
-  get 'careers', to: 'static_pages#careers'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
@@ -13,13 +9,12 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:create]
+      resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
   end
-  
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
